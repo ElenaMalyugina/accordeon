a451a2285958fea98cadbffa0646ca303487e610
Elena Malyugina <malyugina.el@yandex.ru>
2016-11-12 22:33:03 +0300

    аккордеон на jquery

---
+++ source/js/app.js
@@ -0,0 +1,29 @@
+//с джейквери
+$(document).ready(function () {
+
+    $('.acco__section .content').hide();
+    $('.acco__section_active .content').show();
+
+    $('.header').on('click', function (e) {
+        e.preventDefault();
+
+    var $this=$(this),
+        section=$this.closest('.acco__section'),
+        content=section.find('.content'),
+        contentPass=section.siblings().find('.content')
+        ;
+
+    if(!section.hasClass('acco__section_active')){
+
+        content.slideDown();
+        contentPass.slideUp();
+
+        section.addClass('acco__section_active')
+            .siblings()
+            .removeClass('acco__section_active');
+
+    }
+
+    });
+
+});
