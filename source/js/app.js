be35ad68a9b537a18a8763ca5e7316d5424912d0
Elena Malyugina <malyugina.el@yandex.ru>
2016-11-13 23:07:20 +0300

    добавлен слайдер

--- source/js/app.js
+++ source/js/app.js
@@ -27,6 +27,8 @@ $(document).ready(function () {
 
         });
     }());
+
+
     //slideshow
     (function () {
         $('.slideshow').on('click', function(e){
@@ -49,11 +51,114 @@ $(document).ready(function () {
             }
         });
 
+    }());
+
+    //tabs
+    (function () {
+
+        $('.tab__li').on('click', function (e) {
+            e.preventDefault;
+
+            var $this=$(this),
+                container=$this.closest('.tabs__container'),
+                tabs=container.find('tab__li'),
+                ndx=$this.index(),
+                content=container.find('.tabContent__li'),
+                activeContent=content.eq(ndx)
+                ;
+
+            if(!$this.hasClass('tab__li_active')){
+                $this.addClass('tab__li_active')
+                    .siblings()
+                    .removeClass('tab__li_active');
+            }
+
+            activeContent.addClass('tabContent__li_active')
+                .siblings()
+                .removeClass('tabContent__li_active');
 
 
+
+        })
 
     }());
 
+    //слайдер
 
+    (function () {
+        var i=1;
+        var flag=true;
+
+
+            $('.arrow').on('click', function (e) {
+                e.preventDefault;
+
+
+            if(flag==true) {
+
+                flag = false;
+
+                var $this = $(this),
+                    container = $this.siblings('.slider'),
+                    slides = container.find('.slider__li'),
+                    activeSlide = slides.filter('.slider__li_active');
+
+
+                if ($this.hasClass('arrow__right')) {
+
+                    if (i < slides.length) {
+
+                        activeSlide.next().animate({'left': '0%'});
+                        activeSlide.animate({'left': '100%'});
+                        slides.css({'left': '-100%'});
+
+                        activeSlide.next().addClass('slider__li_active')
+                            .siblings().removeClass('slider__li_active');
+                        i++;
+                    }
+                    else {
+                        i = 0;
+                        activeSlide.animate({'left': '100%'});
+                        slides.first().animate({'left': '0%'});
+                        slides.css({'left': '-100%'});
+
+                        slides.first().addClass('slider__li_active')
+                            .siblings().removeClass('slider__li_active');
+
+                        i++;
+
+                    }
+                }
+                else {
+                    if (i > 1) {
+
+                        activeSlide.prev().animate({'left': '0%'});
+                        activeSlide.animate({'left': '-100%'});
+                        slides.css({'left': '100%'});
+
+                        activeSlide.prev().addClass('slider__li_active')
+                            .siblings().removeClass('slider__li_active');
+                        i--;
+                    }
+                    else {
+                        i = slides.length;
+                        activeSlide.animate({'left': '-100%'});
+                        slides.last().animate({'left': '0%'});
+                        slides.css({'left': '100%'});
+
+                        slides.last().addClass('slider__li_active')
+                            .siblings().removeClass('slider__li_active');
+
+
+                    }
+                }
+
+                setTimeout(function(){flag=true;}, 500);
+
+            }
+
+            });
+
+    }());
 
 });
